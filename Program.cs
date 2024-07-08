700862f29dfcf9e4b2f520688c3c4c8bf3da04af
ferwindev <fernando.arias@numrot.com>
2024-07-08 10:54:53 -0500

    Adding https config

--- Program.cs
+++ Program.cs
@@ -114,13 +114,23 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+
 using (var scope = app.Services.CreateScope())
 {
   var context = scope.ServiceProvider.GetRequiredService<ApiDbContext>();
   context.Database.Migrate();
 }
 
-//app.UseHttpsRedirection();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+  app.UseExceptionHandler("/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
 
 app.UseCors("main");
 
